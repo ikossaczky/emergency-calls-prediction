cee41d98d2dc7b965986c927c7d9bd2db2532e58
Igor Kossaczky <ikossaczky@protonmail.com>
2022-04-26 20:05:53 +0200

    adapt for sklearn pipeline compatibility

--- data_pipeline.py
+++ data_pipeline.py
@@ -1,7 +1,10 @@
 import numpy as np
 import pandas as pd
 from collections.abc import Iterable
-from typing import Union
+from typing import Union, Tuple
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.pipeline import Pipeline
+from sklearn.tree import DecisionTreeRegressor
 
 DATA_PATH = "./data/dataset.csv"
 # TODO: test if data is really from the intervals
@@ -42,9 +45,19 @@ def get_data(path: str = DATA_PATH, dtype: type = np.int16):
 
 # TODO: test if data is splited correctly (corect years in trian and val and test set), matching of shapes
 def split_data(data: pd.DataFrame,
-               test_years: list[int],
                train_years: list[int],
-               val_ratio: float):
+               test_years: list[int],
+               val_ratio: float,
+               target_column: str,
+               columns_to_drop: list[str],
+               ) -> tuple[tuple[pd.DataFrame, np.ndarray], ...]:
+    def _input_target_split(data: pd.DataFrame) -> tuple[pd.DataFrame, pd.Series]:
+        y = data[target_column].values
+        if target_column not in columns_to_drop:
+            columns_to_drop.append(target_column)
+        x = data.drop(columns_to_drop, axis=1)
+        return x, y
+
     test_set = data[data["year"].isin(test_years)].reset_index(drop=True)
     train_val_set = data[data["year"].isin(train_years)].reset_index(drop=True)
 
@@ -55,17 +68,21 @@ def split_data(data: pd.DataFrame,
     train_set = train_val_set.iloc[train_samples, :].reset_index(drop=True)
     val_set = train_val_set.iloc[val_samples, :].reset_index(drop=True)
 
-    return train_set, val_set, test_set
+    return tuple(_input_target_split(df) for df in (train_set, val_set, test_set))
 
 
-class BaseProcessor:
+class BaseProcessor(BaseEstimator, TransformerMixin):
     """
     Base class for data Processing. Also serves as dummy postprocessor returning the dataframe itself.
     Follows the informal sklearn transformer protokol.
     Also, any sklearn transformer can be used as postprocessor.
     """
 
-    def transform(self, df: pd.DataFrame) -> np.ndarray:
+    def fit(self, df: pd.DataFrame, y=None):
+        """no fittting needed for the BaseProcessor"""
+        return self
+
+    def transform(self, df: pd.DataFrame, y=None) -> np.ndarray:
         return df.values
 
     def __call__(self, df: pd.DataFrame):
@@ -84,7 +101,7 @@ class OneHotEncoder(BaseProcessor):
 
         self.onehot_vectors = np.eye(np.sum(self.interval_lens))
 
-    def transform(self, df: pd.DataFrame) -> np.ndarray:
+    def transform(self, df: pd.DataFrame, y=None) -> np.ndarray:
         assert set(self.columns).issubset(set(df.columns)), \
             "columns missing in dataframe: {}".format(list(set(self.columns) - set(df.columns)))
 
@@ -95,100 +112,41 @@ class OneHotEncoder(BaseProcessor):
         return one_hots
 
 
-class DataPipeline:
-    """
-    DataPipeline prepares the data for training or prediction:
-    """
+class ColumnSubset(BaseProcessor):
+    def __init__(self, columns: list[str], drop=False):
+        self.columns = columns
+        self.drop = drop
 
-    def __init__(self,
-                 target_column: str,
-                 columns_to_drop: list[str],
-                 processors: Union[list[BaseProcessor], BaseProcessor, None]):
-
-        """
-        :param target_column: which data columns should be used as target
-        :param columns_to_drop: which other columns should be dropped from the training data
-        :param postprocessor: optional function which can further process the model input
-        """
-        self.target_column = target_column
-        self.columns_to_drop = columns_to_drop
-        self.processors = processors
-
-        if self.processors is None:
-            self.processors = [BaseProcessor()]
-
-        if not isinstance(self.processors, Iterable):
-            self.processors = [processors]
-
-        # target column should be dropped from the input data in any case
-        if not self.target_column in self.columns_to_drop:
-            self.columns_to_drop.append(self.target_column)
-
-    def _input_target_split(self, data: pd.DataFrame) -> tuple[pd.DataFrame, np.ndarray]:
-        y = data[self.target_column].values
-        x = data.drop(self.columns_to_drop, axis=1)
-        return x, y
-
-    def fit(self, data: pd.DataFrame):
-        x, _ = self._input_target_split(data)
-        for processor in self.processors:
-            if hasattr(processor, "fit"):
-                processor.fit(x)
-
-    def transform(self, data: pd.DataFrame) -> tuple[Union[list[np.ndarray], np.ndarray], np.ndarray]:
-        x, y = self._input_target_split(data)
-        x_transformed = [processor.transform(x) for processor in self.processors]
-        if len(x_transformed) == 1:
-            x_transformed = x_transformed[0]
-        return x_transformed, y
-
-    def __call__(self, data):
-        return self.transform(data)
+    def transform(self, df: pd.DataFrame, y=None) -> pd.DataFrame:
+        if self.drop:
+            return df.drop(self.columns, axis=1)
+        else:
+            return df[self.columns].copy()
 
 
-# y = merged.loc[:, "count"].values
-#
-# xdf = merged.drop(["year", "count"], axis=1)
-#
-# encoder = OneHotEncoder(INTERVAL_DICT)
-# x = encoder.transform(xdf)
+dummy_estimator = Pipeline([("preprocessing", ColumnSubset(["month", "day", "hour"])),
+                            ("model", DecisionTreeRegressor())])
 
 if __name__ == "__main__":
-
-    data = get_data()
-
-    train_df, val_df, test_df = split_data(data, test_years=[2021, 2022],train_years=list(range(2017, 2021)),
-                                           val_ratio=0.1)
-
-    # data_pipeline = DataPipeline(target_column="count", processors=OneHotEncoder(INTERVAL_DICT))
-    data_pipeline = DataPipeline(target_column="count", columns_to_drop=["year"], processors=BaseProcessor())
-
-    data_pipeline.fit(train_df)
-
-    (x, y), (xv, yv), (xt, yt) = [data_pipeline(df) for df in [train_df, val_df, test_df]]
-
     from sklearn.ensemble import AdaBoostRegressor, RandomForestRegressor, GradientBoostingRegressor, StackingRegressor
     from sklearn.tree import DecisionTreeRegressor
     from sklearn.pipeline import Pipeline
-    models = [AdaBoostRegressor(), RandomForestRegressor(max_depth=5), GradientBoostingRegressor(), DecisionTreeRegressor()]
-    # for model in models:
-    #     print(model.fit(x,y).score(xv, yv))
 
-    df = pd.DataFrame(x)
-    df["y"] = y
+    data = get_data()
 
-    model = models[0]
-    pip = Pipeline([("data_pipeline", data_pipeline), ("model", model)])
-    pip.fit(x,y)
+    (x, y), (xv, yv), (xt, yt) = split_data(data,
+                                            train_years=list(range(2017, 2021)),
+                                            test_years=[2021, 2022],
+                                            val_ratio=0.0,
+                                            target_column="count",
+                                            columns_to_drop=["year"])
+    model = GradientBoostingRegressor()
+    preprocessor = BaseProcessor()
+    pipeline = Pipeline([("preprocessing", preprocessor), ("model", model)])
+    pipeline.fit(x, y)
+    print(pipeline.score(xt, yt))
 
-    # model = StackingRegressor(estimators=[(m.__class__.__name__, m) for m in models])
-    # print(model.fit(x,y).score(xv,yv))
-
-
-    # print(merged.max(axis=0))
-    # print(merged.min(axis=0))
-
-    # max_dict = {"month"}
+    print(dummy_estimator.fit(x, y).score(xt, yt))
 
     # todo substract min
     # todo add column names (in  bash script)
